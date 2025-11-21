8cfb25bd6032d878948606e6256a6e6a929461d9
Andrew Hemming <andrew@footprintit.net>
2025-11-21 00:03:35 +0000

    Initial release

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terraform module for CoreDNS
+
+Installs CoreDNS on an EKS cluster using Helm.
+
+Avoids the 10-minute new cluster provisioning bug with the equivalent EKS add on.
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "chart_version" {
+  description = "Helm chart version to install"
+  type        = string
+  default     = "1.16.1"
+}
+
+variable "cluster_ip" {
+  description = "The cluster IP for the kube-dns service."
+  type        = string
+  default     = "172.20.0.10"
+}
+
+variable "replica_count" {
+  description = "The number of CoreDNS replicas."
+  type        = number
+  default     = 2
+}
